74f4a8651f547987d3ac23c2e27ce0714c162339
CurleyDong2023 <curleydong@163.com>
2023-07-06 19:46:46 +0800

    1) Add Get SCC with Tarjan 2) Add Get LCA with Tarjan
    Attention : Even these two algorithm's names are same, they are different algorithm

--- 数据结构/LCA.cpp
+++ 数据结构/LCA.cpp
@@ -1,6 +1,9 @@
 // Lowest Common Ancestor (LCA)
 // problem url : https://www.luogu.com.cn/problem/P3379
-
+/*
+    1、倍增方法
+    建立O(nlogn) 单次查询O(logn)
+*/
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -70,6 +73,74 @@ signed main() {
         int u, v;
         cin >> u >> v;
         cout << LCA(u, v) << "\n";
+    }
+
+    return 0;
+}
+/*
+    2、Tarjan方法
+    建立O(n) m次查询共 ~ O(n + m)
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+using i64 = long long;
+
+constexpr int N = 5E5 + 10;
+
+vector<int> edge[N];
+vector<pair<int, int> > query[N];
+int fa[N], ans[N], vis[N];
+
+int find(int x) {
+    return x == fa[x] ? x : fa[x] = find(fa[x]); 
+}
+
+void Tarjan(int u) {
+    vis[u] = 1;
+    for (auto v : edge[u]) {
+        if (!vis[v]) {
+            Tarjan(v);
+            fa[v] = u;
+        }
+    }
+
+    for (auto [v, i] : query[u]) {
+        if (vis[v]) {
+            ans[i] = find(v);
+        }
+    }
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n, m, s;
+    cin >> n >> m >> s;
+
+    for (int i = 1; i <= n - 1; i++) {
+        int u, v;
+        cin >> u >> v;
+        edge[u].push_back(v);
+        edge[v].push_back(u);
+    }
+    
+    for (int i = 1; i <= m; i++) {
+        int u, v;
+        cin >> u >> v;
+        query[u].push_back({v, i});
+        query[v].push_back({u, i});
+    }
+
+    for (int i = 1; i <= n; i++) {
+        fa[i] = i;
+    }
+
+    Tarjan(s);
+
+    for (int i = 1; i <= m; i++) {
+        cout << ans[i] << "\n";
     }
 
     return 0;
